fdc21f627f57590bcc369547e99af01afe50f079
Sbats <simon.bats12@gmail.com>
2016-06-22 18:42:03 +0200

    Create library route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 
 import { HomeComponent } from './home';
+import { LibraryComponent } from './library';
 
 export const routes: RouterConfig = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'library', component: LibraryComponent },
 ];
 
 export const APP_ROUTER_PROVIDERS = [
