1f93e3afebb9302dda4ccaabeb6dbfd0e4073399
Sbats <simon.bats12@gmail.com>
2016-06-22 23:54:41 +0200

    Comics list component should be passed list instead of fetching it

--- src/app/comics/list/comics-list.component.ts
+++ src/app/comics/list/comics-list.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { MarvelService } from '../../shared';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mrs-comics-list',
   template: require('./comics-list.component.html'),
   styles: [require('./comics-list.component.scss')]
 })
-export class ComicsList implements OnInit {
-  comics: any[];
-
-  constructor(public marvelService: MarvelService) {}
-
-  ngOnInit(): void {
-    this.marvelService.getComicsList()
-      .subscribe((res: any) => {
-        this.comics = res.results;
-      });
-  }
+export class ComicsListComponent {
+  @Input() list: any[];
 }
