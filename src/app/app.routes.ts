f9d6f567dd658f432e2bed42dfd89811acdf9ca8
Sbats <simon.bats12@gmail.com>
2016-06-24 17:32:37 +0200

    Create Library component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import { HomeComponent } from './home';
 import { LibraryComponent } from './library';
+import { CollectionComponent } from './collection';
 
 export const routes: RouterConfig = [
   { path: '', component: HomeComponent },
   { path: 'library', component: LibraryComponent },
+  { path: 'collection', component: CollectionComponent },
 ];
 
 export const APP_ROUTER_PROVIDERS = [
---
+++ src/app/collection/collection.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mrs-collection',
+  template: require('./collection.component.html'),
+  styles: [require('./collection.component.scss')]
+})
+export class CollectionComponent {
+}
