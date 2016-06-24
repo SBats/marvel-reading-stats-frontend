223ea671e5fae9b46d6ef87a6084e13520c4c2eb
Sbats <simon.bats12@gmail.com>
2016-06-24 14:58:16 +0200

    Rename models to interfaces and create another models file

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,5 +2,6 @@ export * from './api.service';
 export * from './marvel.service';
 export * from './mrs.service';
 export * from './storage.service';
+export * from './interfaces';
 export * from './models';
 export * from './providers';
---
+++ src/app/shared/interfaces.ts
@@ -0,0 +1,140 @@
+export interface ComicDataWrapper {
+  code?: number;
+  status?: string;
+  copyright?: string;
+  attributionText?: string;
+  attributionHTML?: string;
+  data?: ComicDataContainer;
+  etag?: string;
+}
+
+export interface ComicDataContainer {
+  offset?: number;
+  limit?: number;
+  total?: number;
+  count?: number;
+  results?: Comic[];
+}
+
+export interface Comic {
+  id?: number;
+  digitalId?: number;
+  title?: string;
+  issueNumber?: number;
+  variantDescription?: string;
+  description?: string;
+  modified?: Date;
+  isbn?: string;
+  upc?: string;
+  diamondCode?: string;
+  ean?: string;
+  issn?: string;
+  format?: string;
+  pageCount?: number;
+  textObjects?: TextObject[];
+  resourceURI?: string;
+  urls?: ComicUrl[];
+  series?: SeriesSummary;
+  variants?: ComicSummary[];
+  collections?: ComicSummary[];
+  collectedIssues?: ComicSummary[];
+  dates?: ComicDate[];
+  prices?: ComicPrice[];
+  thumbnail?: ComicImage;
+  images?: ComicImage[];
+  creators?: CreatorList;
+  characters?: CharacterList;
+  stories?: StoryList;
+  events?: EventList;
+}
+
+export interface ComicUrl {
+  type?: string;
+  url?: string;
+}
+
+export interface TextObject {
+  type?: string;
+  language?: string;
+  text?: string;
+}
+
+export interface SeriesSummary {
+  resourceURI?: string;
+  name?: string;
+}
+
+export interface ComicSummary {
+  resourceURI?: string;
+  name?: string;
+}
+
+export interface ComicDate {
+  type?: string;
+  date?: Date;
+}
+
+export interface ComicPrice {
+  type?: string;
+  price?: number;
+}
+
+export interface ComicImage {
+  path?: string;
+  extension?: string;
+}
+
+export interface CreatorList {
+  available?: number;
+  returned?: number;
+  collectionURI?: string;
+  items?: CreatorSummary[];
+}
+
+export interface CreatorSummary {
+  resourceURI?: string;
+  name?: string;
+  role?: string;
+}
+
+export interface CharacterList {
+  available?: number;
+  returned?: number;
+  collectionURI?: string;
+  items?: CharacterSummary[];
+}
+
+export interface CharacterSummary {
+  resourceURI?: string;
+  name?: string;
+  role?: string;
+}
+
+export interface StoryList {
+  available?: number;
+  returned?: number;
+  collectionURI?: string;
+  items?: StorySummary[];
+}
+
+export interface StorySummary {
+  resourceURI?: string;
+  name?: string;
+  type?: string;
+}
+
+export interface EventList {
+  available?: number;
+  returned?: number;
+  collectionURI?: string;
+  items?: EventSummary[];
+}
+
+export interface EventSummary {
+  resourceURI?: string;
+  name?: string;
+}
+
+export interface UserData {
+  comics: Comic[]
+}
