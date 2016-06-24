ae46e8ba0963eb94393136ca30f19376067db7a6
Sbats <simon.bats12@gmail.com>
2016-06-24 17:35:30 +0200

    Display message on Collection component depending on userHasCollection

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { MRSService } from '../shared';
 
 @Component({
   selector: 'mrs-collection',
   template: require('./collection.component.html'),
-  styles: [require('./collection.component.scss')]
+  styles: [require('./collection.component.scss')],
+  directives: [...ROUTER_DIRECTIVES]
 })
 export class CollectionComponent {
+  userHasCollection: boolean = false;
+
+  constructor(private _mrsService: MRSService) {
+  }
+
+  ngOnInit() {
+    this.userHasCollection = this._mrsService.userHasCollection;
+  }
 }
