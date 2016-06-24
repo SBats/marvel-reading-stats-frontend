48a8fc36add7d52051d1aeb365d304a79988e269
Sbats <simon.bats12@gmail.com>
2016-06-24 18:05:49 +0200

    Display and manage user collection in Collection component

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
-import { MRSService } from '../shared';
+import { MRSService, UserData } from '../shared';
+import { ComicsListComponent } from '../comics/list';
 
 @Component({
   selector: 'mrs-collection',
   template: require('./collection.component.html'),
   styles: [require('./collection.component.scss')],
-  directives: [...ROUTER_DIRECTIVES]
+  directives: [
+    ...ROUTER_DIRECTIVES,
+    ComicsListComponent
+  ]
 })
 export class CollectionComponent {
   userHasCollection: boolean = false;
+  collection: any[] = [];
 
   constructor(private _mrsService: MRSService) {
   }
 
   ngOnInit() {
+    this._mrsService.userData.subscribe((data: any) => {
+      this.collection = Array.from(data.comics.values());
+      this.collection.map(element => element.isInCollection = true);
+    });
     this.userHasCollection = this._mrsService.userHasCollection;
+  }
+
+  addComicToCollection(comic) {
+    this._mrsService.addComic(comic);
+  }
+
+  removeComicFromCollection(comic) {
+    this._mrsService.removeComic(comic);
   }
 }
