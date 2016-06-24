001f046700f89e4bd2160a6eca5a27d04f1d2630
Sbats <simon.bats12@gmail.com>
2016-06-24 11:45:52 +0200

    Use mrsService on home to know if user has a collection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { MRSService } from '../shared';
 
 @Component({
   selector: 'mrs-home',
+  directives: [...ROUTER_DIRECTIVES],
   template: require('./home.component.html'),
   styles: [require('./home.component.scss')]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  userHasCollection: boolean = false;
+
+  constructor(private _mrsService: MRSService) {
+  }
+
+  ngOnInit() {
+    this._mrsService.userData
+      .subscribe(data => {
+        this.userHasCollection = data.comics && data.comics.length > 0
+      });
+  }
+
 }
