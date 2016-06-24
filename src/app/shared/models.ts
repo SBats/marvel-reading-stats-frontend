93957dc93a7cb9e4d6b8fb8c937aab825c674950
Sbats <simon.bats12@gmail.com>
2016-06-24 16:47:40 +0200

    Clean models

--- src/app/shared/models.ts
+++ src/app/shared/models.ts
@@ -1,20 +1,9 @@
 import { MarvelComic } from './interfaces';
 
 export class Comic implements MarvelComic {
-  id: number;
   isInCollection: boolean = false;
-
-  constructor() {
-  }
-
-
 }
 
 export class ComicsCollection {
   collection: Comic[];
-
-  constructor() {
-  }
-
-
 }
