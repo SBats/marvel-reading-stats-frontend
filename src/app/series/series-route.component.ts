7a57fa7152589c2ca5a7c8f408fb5ae0e7653a0c
Sbats <simon.bats12@gmail.com>
2016-06-26 23:13:34 +0200

    Harmonize pages titles

--- src/app/series/series-route.component.ts
+++ src/app/series/series-route.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ROUTER_DIRECTIVES, ActivatedRoute } from '@angular/router';
 import { MarvelService } from '../shared';
 
 import { SeriesListComponent } from '../series/list';
@@ -9,6 +9,7 @@ import { SeriesListComponent } from '../series/list';
   template: require('./series-route.component.html'),
   styles: [require('./series-route.component.scss')],
   directives: [
+    ROUTER_DIRECTIVES,
     SeriesListComponent
   ]
 })
