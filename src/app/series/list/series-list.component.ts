df70476400312125f3002a356c2e73bb934952a7
Sbats <simon.bats12@gmail.com>
2016-06-26 21:03:59 +0200

    Series list should link to serie id instead of using callback

--- src/app/series/list/series-list.component.ts
+++ src/app/series/list/series-list.component.ts
@@ -1,25 +1,15 @@
 import {
   Component,
-  Input,
-  Output,
-  EventEmitter
+  Input
 } from '@angular/core';
+import { ROUTER_DIRECTIVES }    from '@angular/router';
 
 @Component({
   selector: 'mrs-series-list',
   template: require('./series-list.component.html'),
-  styles: [require('./series-list.component.scss')]
+  styles: [require('./series-list.component.scss')],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class SeriesListComponent {
   @Input() list: any[];
-  @Output() serieClick: EventEmitter<any>;
-
-  constructor() {
-    this.serieClick = new EventEmitter();
-  }
-
-  serieAction(serie) {
-    this.serieClick.emit(serie);
-  }
-
 }
