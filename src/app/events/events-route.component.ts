f88fccd401324dfbbb28d0f8ad3a137a3afd19df
Sbats <simon.bats12@gmail.com>
2016-06-26 20:06:32 +0200

    Update library subroutes to load mocked series list

--- src/app/events/events-route.component.ts
+++ src/app/events/events-route.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarvelService } from '../shared';
+
+import { SeriesListComponent } from '../series/list';
 
 @Component({
   selector: 'mrs-events-route',
   template: require('./events-route.component.html'),
-  styles: [require('./events-route.component.scss')]
+  styles: [require('./events-route.component.scss')],
+  directives: [
+    SeriesListComponent
+  ]
 })
-export class EventsRouteComponent {
+export class EventsRouteComponent implements OnInit {
+  elements: any[] = [];
+
+  constructor(
+    private _marvelService: MarvelService
+  ) {}
+
+  ngOnInit(): void {
+    this._marvelService.getSeries()
+      .subscribe((res) => {
+        this.elements = res.data.results;
+      });
+  }
+
+  navigateToSerie(serie) {
+    console.log(serie);
+  }
 }
--- src/app/series/series-route.component.ts
+++ src/app/series/series-route.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarvelService } from '../shared';
+
+import { SeriesListComponent } from '../series/list';
 
 @Component({
   selector: 'mrs-series-route',
   template: require('./series-route.component.html'),
-  styles: [require('./series-route.component.scss')]
+  styles: [require('./series-route.component.scss')],
+  directives: [
+    SeriesListComponent
+  ]
 })
-export class SeriesRouteComponent {
+export class SeriesRouteComponent implements OnInit {
+  elements: any[] = [];
+
+  constructor(
+    private _marvelService: MarvelService
+  ) {}
+
+  ngOnInit(): void {
+    this._marvelService.getSeries()
+      .subscribe((res) => {
+        this.elements = res.data.results;
+      });
+  }
+
+  navigateToSerie(serie) {
+    console.log(serie);
+  }
 }
