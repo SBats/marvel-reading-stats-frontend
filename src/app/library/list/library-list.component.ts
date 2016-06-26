585a10b9ff4b96ae39fa11903787f9a31a0ebb7f
Sbats <simon.bats12@gmail.com>
2016-06-27 01:34:52 +0200

    Add start by letter filtering system on library list view

--- src/app/library/list/library-list.component.ts
+++ src/app/library/list/library-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ROUTER_DIRECTIVES, ActivatedRoute } from '@angular/router';
+import { ROUTER_DIRECTIVES, ActivatedRoute, Router } from '@angular/router';
 import { MarvelService } from '../../shared';
 
 import { ElementsListComponent } from '../../elements-list';
@@ -16,11 +16,14 @@ import { ElementsListComponent } from '../../elements-list';
 export class LibraryListComponent implements OnInit {
   elements: any[] = [];
   libraryType: string;
+  queryFiltersList: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' , 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  startWithQuery: string = this.queryFiltersList[0];
   private subscribers: any[] = [];
 
   constructor(
     private marvelService: MarvelService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -29,6 +32,15 @@ export class LibraryListComponent implements OnInit {
         .params
         .subscribe(params => {
           this.libraryType = params['type'];
+        })
+    );
+
+    this.subscribers.push(
+      this.router
+        .routerState
+        .queryParams
+        .subscribe(params => {
+          this.startWithQuery = params['startwith'] || this.queryFiltersList[0];
         })
     );
 
@@ -42,5 +54,10 @@ export class LibraryListComponent implements OnInit {
 
   ngOnDestroy() {
     this.subscribers.map(sb => sb.unsubscribe());
+  }
+
+  selectQueryFilter(ev, filter) {
+    ev.preventDefault();
+    this.router.navigate(['.'], {queryParams: { startwith: filter}});
   }
 }
