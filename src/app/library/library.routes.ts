38d95a23eb7138afed09fcc1f8c32f8a4cf2c1f3
Sbats <simon.bats12@gmail.com>
2016-06-26 21:02:51 +0200

    Add subroutes to router

--- src/app/library/library.routes.ts
+++ src/app/library/library.routes.ts
@@ -3,6 +3,7 @@ import { RouterConfig } from '@angular/router';
 import { LibraryComponent } from './';
 import { LibraryTypesComponent } from './types';
 import { SeriesRouteComponent } from '../series';
+import { SeriesDetailComponent } from '../series/detail';
 import { CharactersRouteComponent } from '../characters';
 import { CreatorsRouteComponent } from '../creators';
 import { EventsRouteComponent } from '../events';
@@ -12,11 +13,15 @@ export const libraryRoutes: RouterConfig = [
     path: 'library',
     component: LibraryComponent,
     children: [
-      { path: 'series',  component: SeriesRouteComponent },
-      { path: 'characters',  component: CharactersRouteComponent },
-      { path: 'creators',  component: CreatorsRouteComponent },
-      { path: 'events',  component: EventsRouteComponent },
-      { path: '',     component: LibraryTypesComponent },
+      { path: 'series', component: SeriesRouteComponent },
+      { path: 'characters', component: CharactersRouteComponent },
+      { path: 'creators', component: CreatorsRouteComponent },
+      { path: 'events', component: EventsRouteComponent },
+      { path: '', component: LibraryTypesComponent },
+      { path: 'series/:id', component: SeriesDetailComponent },
+      { path: 'characters/:id', component: SeriesDetailComponent },
+      { path: 'creators/:id', component: SeriesDetailComponent },
+      { path: 'events/:id', component: SeriesDetailComponent }
     ]
   }
 ];
