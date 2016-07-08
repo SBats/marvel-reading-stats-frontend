aac824f272e4b4168de130e6fe16be0559a99f04
Sbats <simon.bats12@gmail.com>
2016-07-08 14:55:24 +0200

    Add small unit test to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 
 import { StorageService } from './shared';
 
-describe('App', () => {
+describe('App Component', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEachProviders(() => [
     AppComponent,
@@ -19,5 +19,18 @@ describe('App', () => {
   it('should have a title', inject([AppComponent, StorageService], (app: AppComponent, StorageService) => {
     expect(!!app.title).toEqual(true);
   }));
+
+  it('should have a showNav property to false', inject([AppComponent, StorageService], (app: AppComponent, StorageService) => {
+    expect(app.showNav).toEqual(false);
+  }));
+
+  describe('toggleNav', () => {
+    it('should reverse showNav value', inject([AppComponent, StorageService], (app: AppComponent, StorageService) => {
+      expect(app.showNav).toEqual(false);
+      app.toggleNav();
+      expect(app.showNav).toEqual(true);
+    }));
+  });
+
 
 });
