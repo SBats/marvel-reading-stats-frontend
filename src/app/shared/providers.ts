d2e052d2348e9a0e8bf3ff71156cfd71f91f3a52
Sbats <simon.bats12@gmail.com>
2016-07-08 18:04:29 +0200

    Update custom providers syntax

--- src/app/shared/providers.ts
+++ src/app/shared/providers.ts
@@ -4,8 +4,8 @@ import { StorageService } from './services/storage.service';
 import { MRSService } from './services/mrs.service';
 import { MarvelService } from './services/marvel.service';
 
-export const MRS_APP_PROVIDER: any[] = [].concat(
-  provide(StorageService, {useClass: StorageService}),
-  provide(MRSService, {useClass: MRSService}),
-  provide(MarvelService, {useClass: MarvelService})
-);
+export const MRS_APP_PROVIDER: any[] = [
+  {provide: StorageService, useClass: StorageService},
+  {provide: MRSService, useClass: MRSService},
+  {provide: MarvelService, useClass: MarvelService}
+];
