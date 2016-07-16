fbe8f1d6924f914e9b9526beafb3f2b35cdd8177
Sbats <simon.bats12@gmail.com>
2016-07-16 13:25:58 +0200

    Add information about most viewd character in stats

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
-import { MRSService } from '../shared';
+import { MRSService, MarvelComic } from '../shared';
 import { TimeSpentPipe } from './stats.pipes';
 
 @Component({
@@ -19,6 +19,7 @@ export class StatsComponent implements OnInit, OnDestroy {
   userHasCollection: boolean = false;
   collection: any[] = [];
   private subscribers: any[] = [];
+  favouriteCharacter: string = null;
 
   constructor(private mrsService: MRSService) {
   }
@@ -28,9 +29,35 @@ export class StatsComponent implements OnInit, OnDestroy {
       this.mrsService.userData.subscribe((data: any) => {
         this.collection = Array.from(data.comics.values());
         this.collection.map(element => element.isInCollection = true);
+        this.favouriteCharacter = this.computeFavouriteCharacter(this.collection);
+        console.log(this.favouriteCharacter);
       })
     );
     this.userHasCollection = this.mrsService.userHasCollection;
+  }
+
+  computeFavouriteCharacter(collection: MarvelComic[]): string {
+    const charList = {};
+    let favouriteChar = null;
+    let favouriteCount = 0;
+
+    collection.map(comic => {
+      comic.characters.items.map(char => {
+        if (!charList[char.name]) {
+          charList[char.name] = 1;
+        } else {
+          charList[char.name]++;
+        }
+      });
+    });
+    for (let char in charList) {
+      if (charList[char] > favouriteCount) {
+        favouriteChar = char;
+        favouriteCount = charList[char];
+      }
+    }
+
+    return favouriteChar;
   }
 
   ngOnDestroy() {
