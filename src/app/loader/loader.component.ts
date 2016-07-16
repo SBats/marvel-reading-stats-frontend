3045965fc98241fad125b3326aaa0e38ff37a4cd
Sbats <simon.bats12@gmail.com>
2016-07-16 13:26:56 +0200

    Create a loader component

---
+++ src/app/loader/loader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mrs-loader',
+  template: require('./loader.component.html')
+})
+export class LoaderComponent extends Component {
+}
