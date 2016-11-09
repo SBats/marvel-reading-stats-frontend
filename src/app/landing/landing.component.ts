15c80925ed3b1a3be094651b3e451531b7d9a092
SBats <simon.bats12@gmail.com>
2016-11-10 00:25:55 +0100

    Animate landing and library types pages

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,9 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  trigger,
+  transition,
+  style,
+  state,
+  animate
+} from '@angular/core';
 
 @Component({
   selector: 'mrs-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  host: {
+    '[@routeAnimation]': 'true',
+    '[style.display]': "'block'",
+    '[style.position]': "'absolute'"
+  },
+  animations: [
+    trigger('routeAnimation', [
+      state('*', style({
+        opacity: 1
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0
+        }),
+        animate('400ms ease-out')
+      ]),
+      transition('* => void', animate('400ms ease-out', 
+        style({
+          opacity: 0
+        })
+      ))
+    ])
+  ]
 })
 export class LandingComponent implements OnInit {
 
--- src/app/library/types/types.component.ts
+++ src/app/library/types/types.component.ts
@@ -1,9 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  trigger,
+  transition,
+  style,
+  state,
+  animate
+} from '@angular/core';
 
 @Component({
   selector: 'mrs-types',
   templateUrl: './types.component.html',
-  styleUrls: ['./types.component.scss']
+  styleUrls: ['./types.component.scss'],
+  host: {
+    '[@routeAnimation]': 'true',
+    '[style.display]': "'block'",
+    '[style.position]': "'absolute'"
+  },
+  animations: [
+    trigger('routeAnimation', [
+      state('*', style({
+        opacity: 1
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0
+        }),
+        animate('400ms ease-out')
+      ]),
+      transition('* => void', animate('400ms ease-out', 
+        style({
+          opacity: 0
+        })
+      ))
+    ]),
+    trigger('typeItemAnimation', [
+      state('*', style({
+        opacity: 1
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0
+        }),
+        animate('400ms 400ms ease-out')
+      ]),
+      transition('* => void', animate('400ms 400ms ease-out', 
+        style({
+          opacity: 0
+        })
+      ))
+    ])
+  ],
 })
 export class TypesComponent implements OnInit {
 
@@ -13,9 +60,14 @@ export class TypesComponent implements OnInit {
     {img: '/assets/events-civil_war.jpg', title: 'Events'},
     {img: '/assets/image1-36d32.gif', title: 'Heroes'}
   ];
+  timedTypes: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.types.map((type, index) => {
+      setTimeout(() => this.timedTypes.push(type), 100 * index);
+    });
   }
 
 }
