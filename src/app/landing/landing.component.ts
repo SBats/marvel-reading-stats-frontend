24c990973720e3a5ed5e31b06b954810d2cc48f7
SBats <simon.bats12@gmail.com>
2016-11-11 17:39:50 +0100

    Fix general style and remove route animations

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,5 @@
 import {
   Component,
-  OnInit,
   trigger,
   transition,
   style,
@@ -11,36 +10,6 @@ import {
 @Component({
   selector: 'mrs-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss'],
-  host: {
-    '[@routeAnimation]': 'true',
-    '[style.display]': "'block'",
-    '[style.position]': "'absolute'"
-  },
-  animations: [
-    trigger('routeAnimation', [
-      state('*', style({
-        opacity: 1
-      })),
-      transition('void => *', [
-        style({
-          opacity: 0
-        }),
-        animate('400ms ease-out')
-      ]),
-      transition('* => void', animate('400ms ease-out', 
-        style({
-          opacity: 0
-        })
-      ))
-    ])
-  ]
+  styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class LandingComponent { }
--- src/app/library/types/types.component.ts
+++ src/app/library/types/types.component.ts
@@ -1,6 +1,5 @@
 import {
   Component,
-  OnInit,
   trigger,
   transition,
   style,
@@ -12,28 +11,7 @@ import {
   selector: 'mrs-types',
   templateUrl: './types.component.html',
   styleUrls: ['./types.component.scss'],
-  host: {
-    '[@routeAnimation]': 'true',
-    '[style.display]': "'block'",
-    '[style.position]': "'absolute'"
-  },
   animations: [
-    trigger('routeAnimation', [
-      state('*', style({
-        opacity: 1
-      })),
-      transition('void => *', [
-        style({
-          opacity: 0
-        }),
-        animate('400ms ease-out')
-      ]),
-      transition('* => void', animate('400ms ease-out', 
-        style({
-          opacity: 0
-        })
-      ))
-    ]),
     trigger('typeItemAnimation', [
       state('*', style({
         opacity: 1
@@ -52,22 +30,11 @@ import {
     ])
   ],
 })
-export class TypesComponent implements OnInit {
-
+export class TypesComponent {
   types: any[] = [
-    {img: '/assets/stan-lee.png', title: 'Authors'},
-    {img: '/assets/series-thanos.jpg', title: 'Series'},
-    {img: '/assets/events-civil_war.jpg', title: 'Events'},
-    {img: '/assets/image1-36d32.gif', title: 'Heroes'}
+    {img: '/assets/stan-lee.png', title: 'Authors', url: 'authors'},
+    {img: '/assets/series-thanos.jpg', title: 'Series', url: 'series'},
+    {img: '/assets/events-civil_war.jpg', title: 'Events', url: 'events'},
+    {img: '/assets/image1-36d32.gif', title: 'Heroes', url: 'heroes'}
   ];
-  timedTypes: any[] = [];
-
-  constructor() { }
-
-  ngOnInit() {
-    this.types.map((type, index) => {
-      setTimeout(() => this.timedTypes.push(type), 100 * index);
-    });
-  }
-
 }
