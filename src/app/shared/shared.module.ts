1bb0523d71e92ae2813428e4c70438a651f1538f
SBats <simon.bats12@gmail.com>
2016-11-13 11:56:39 +0100

    Declare and export Login and Subscribe forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule }   from '@angular/forms';
 
 import { ComicComponent } from './comic/comic.component';
 import { ComicsListComponent } from './comics-list/comics-list.component';
 import { FooterComponent } from './footer';
+import { LoginFormComponent } from './login-form';
+import { SubscribeFormComponent } from './subscribe-form';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   declarations: [
     ComicComponent,
     ComicsListComponent,
-    FooterComponent
+    FooterComponent,
+    LoginFormComponent,
+    SubscribeFormComponent
   ],
   exports: [
     CommonModule,
     ComicComponent,
     ComicsListComponent,
-    FooterComponent
+    FooterComponent,
+    LoginFormComponent,
+    SubscribeFormComponent
   ]
 })
 export class SharedModule { }
