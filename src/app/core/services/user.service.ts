9f5b9cb696e7c92425d50c206d213e997ca294a3
SBats <simon.bats12@gmail.com>
2016-11-19 19:47:56 +0100

    Add login method to User service

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import '../../rxjs-operators';
 
 @Injectable()
 export class UserService {
+  token: string = null;
+  constructor(private http: Http) { }
 
-  constructor() { }
+  login(credentials): Promise<any> {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
+    const url = 'http://localhost:8000/api-token-auth/';
+    return this.http.post(url, {
+      username: credentials.username,
+      password: credentials.password
+    }, options)
+      .toPromise()
+      .then((res: Response) => res.json())
+      .then(json => this.token = json.token)
+      .catch((err: Response) => console.error(err));
+  }
 
 }
---
+++ src/app/rxjs-operators.ts
@@ -0,0 +1,6 @@
+// Statics
+import 'rxjs/add/observable/throw';
+
+// Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
