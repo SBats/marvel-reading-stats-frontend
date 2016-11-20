37adbd3dad958192e79106b4aee9888373caa110
SBats <simon.bats12@gmail.com>
2016-11-20 19:03:07 +0100

    Simplify library routes

--- src/app/library/library-routing.module.ts
+++ src/app/library/library-routing.module.ts
@@ -4,9 +4,8 @@ import { TypesComponent } from './types';
 import { ListComponent } from './list';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'types', pathMatch: 'full' },
-  { path: 'types', component: TypesComponent },
-  { path: ':type/list', component: ListComponent }
+  { path: '', component: TypesComponent },
+  { path: ':type', component: ListComponent }
 ];
 
 @NgModule({
