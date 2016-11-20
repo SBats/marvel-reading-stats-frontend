651d18e39385394d5ab12840037cadcbddbf449e
SBats <simon.bats12@gmail.com>
2016-11-20 20:01:54 +0100

    Improve library list view

--- src/app/library/list/list.component.ts
+++ src/app/library/list/list.component.ts
@@ -7,7 +7,12 @@ import {
   transition,
   animate
 } from '@angular/core';
+import {
+  Router,
+  ActivatedRoute
+} from '@angular/router';
 import { MarvelService } from '../../core';
+import '../../rxjs-operators';
 
 @Component({
   selector: 'mrs-list',
@@ -15,14 +20,78 @@ import { MarvelService } from '../../core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  alphabet: string[] = [
-    '#', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  results: any[] = [];
+  loading: boolean = true;
+  libraryType: string;
+  queryFiltersList: string[] = [
+    '#', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
   ];
-  constructor(private marvelService: MarvelService) { }
+  startWithQuery: string = null;
+  private subscribers: any[] = [false];
+
+  constructor(
+    private marvelService: MarvelService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.subscribers.push(
+      this.route
+        .params
+        .subscribe(params => {
+          this.libraryType = params['type'];
+          this.loadList();
+        })
+    );
+
+    this.subscribers.push(
+      this.route
+        .queryParams
+        .subscribe(params => {
+          this.startWithQuery = params['startwith'] || this.queryFiltersList[0];
+
+          if (this.libraryType) {
+            this.loadList();
+          }
+        })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscribers.map(sb => sb.unsubscribe());
+  }
+
+  selectQueryFilter(ev, filter) {
+    ev.preventDefault();
+    this.startWithQuery = filter;
+    this.updateQueries();
+  }
+
+  updateQueries() {
+    let params: any = {};
+    if (this.startWithQuery) {
+      params.startwith = this.startWithQuery;
+    }
+    this.router.navigate(['library', this.libraryType], {queryParams: params});
+  }
+
+  loadList() {
+    this.loading = true;
+
+    if (this.subscribers[0] && this.subscribers[0].unsubscribe) {
+      this.subscribers[0].unsubscribe();
+    }
+
+    this.subscribers[0] = this.marvelService.getTypeList(
+      this.libraryType,
+      this.startWithQuery
+    ).subscribe((res: any[]) => {
+      this.results = res['results'];
+      this.loading = false;
+    });
   }
 
 }
