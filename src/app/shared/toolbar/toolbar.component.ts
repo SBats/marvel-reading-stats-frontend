3e56af6da6ff128ff48a71951ad0b8a5e0b4c9e6
SBats <simon.bats12@gmail.com>
2016-11-24 01:16:40 +0100

    Create toolbar component

---
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mrs-toolbar',
+  template: `<ng-content></ng-content>`,
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent {}
