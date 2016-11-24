5c5f14eba6e04154f1b1f47e23a11d284c473d78
SBats <simon.bats12@gmail.com>
2016-11-24 01:17:06 +0100

    Register toolbar and breadcrumb components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ComicComponent } from './comic/comic.component';
@@ -10,10 +11,13 @@ import { SubscribeFormComponent } from './subscribe-form';
 import { ConnectionModalComponent } from './connection-modal';
 import { LoaderComponent } from './loader';
 import { TypeListComponent } from './type-list';
+import { ToolbarComponent } from './toolbar';
+import { BreadcrumbComponent } from './breadcrumb';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule.forChild([]),
     ReactiveFormsModule
   ],
   declarations: [
@@ -24,7 +28,9 @@ import { TypeListComponent } from './type-list';
     SubscribeFormComponent,
     ConnectionModalComponent,
     LoaderComponent,
-    TypeListComponent
+    TypeListComponent,
+    ToolbarComponent,
+    BreadcrumbComponent
   ],
   exports: [
     CommonModule,
@@ -35,7 +41,9 @@ import { TypeListComponent } from './type-list';
     SubscribeFormComponent,
     ConnectionModalComponent,
     LoaderComponent,
-    TypeListComponent
+    TypeListComponent,
+    ToolbarComponent,
+    BreadcrumbComponent 
   ]
 })
 export class SharedModule { }
