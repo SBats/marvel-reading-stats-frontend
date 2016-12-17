39cca080748ec850782dee023f07609af50ed0ef
SBats <simon.bats12@gmail.com>
2016-12-17 23:49:58 +0100

    Add library detail route

--- src/app/library/library-routing.module.ts
+++ src/app/library/library-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TypesComponent } from './types';
 import { ListComponent } from './list';
+import { DetailComponent } from './detail';
 
 export const routes: Routes = [
   { path: '', component: TypesComponent },
-  { path: ':type', component: ListComponent }
+  { path: ':type', component: ListComponent },
+  { path: ':type/:element', component: DetailComponent }
 ];
 
 @NgModule({
